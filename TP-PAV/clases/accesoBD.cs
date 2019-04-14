dbd2c9c3ca33681d86f869146935b085e689fbd9
LittleMok <mokivanalejandro@gmail.com>
2019-04-14 16:55:24 -0300

    Revert "Editado .gitignore"
    This reverts commit cd81e4761e597791cde7effbd1f1c8b83922b67f.

--- TP-PAV/clases/accesoBD.cs
+++ TP-PAV/clases/accesoBD.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using System.Data;
 using System.Data.OleDb;
-
 namespace TP_PAV.clases
 {
     class accesoBD
