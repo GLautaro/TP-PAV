37272c400e704769cd16929e9f9eccba31564b62
GLautaro <glautaroivan@gmail.com>
2019-04-20 11:12:51 -0300

    Modificando Front-end del Escritorio y Forms
    Diseño utilizando Docks. Creando una side-bar

--- TP-PAV/formularios/escritorio.Designer.cs
+++ TP-PAV/formularios/escritorio.Designer.cs
@@ -28,12 +28,211 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Escritorio));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SidePanel = new System.Windows.Forms.Panel();
+            this.btn_menuReportes = new System.Windows.Forms.Button();
+            this.btn_menuEstadisticas = new System.Windows.Forms.Button();
+            this.btn_menuFranquicias = new System.Windows.Forms.Button();
+            this.btn_menuProductos = new System.Windows.Forms.Button();
+            this.btn_menuVendedores = new System.Windows.Forms.Button();
+            this.btn_menuPedidos = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(39)))), ((int)(((byte)(40)))));
+            this.panel1.Controls.Add(this.SidePanel);
+            this.panel1.Controls.Add(this.btn_menuReportes);
+            this.panel1.Controls.Add(this.btn_menuEstadisticas);
+            this.panel1.Controls.Add(this.btn_menuFranquicias);
+            this.panel1.Controls.Add(this.btn_menuProductos);
+            this.panel1.Controls.Add(this.btn_menuVendedores);
+            this.panel1.Controls.Add(this.btn_menuPedidos);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(190, 499);
+            this.panel1.TabIndex = 0;
+            // 
+            // SidePanel
+            // 
+            this.SidePanel.BackColor = System.Drawing.Color.YellowGreen;
+            this.SidePanel.Location = new System.Drawing.Point(3, 56);
+            this.SidePanel.Name = "SidePanel";
+            this.SidePanel.Size = new System.Drawing.Size(10, 54);
+            this.SidePanel.TabIndex = 3;
+            // 
+            // btn_menuReportes
+            // 
+            this.btn_menuReportes.FlatAppearance.BorderSize = 0;
+            this.btn_menuReportes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_menuReportes.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_menuReportes.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn_menuReportes.Location = new System.Drawing.Point(3, 341);
+            this.btn_menuReportes.Name = "btn_menuReportes";
+            this.btn_menuReportes.Size = new System.Drawing.Size(184, 54);
+            this.btn_menuReportes.TabIndex = 8;
+            this.btn_menuReportes.Text = "Reportes";
+            this.btn_menuReportes.UseVisualStyleBackColor = true;
+            this.btn_menuReportes.Click += new System.EventHandler(this.btn_menuReportes_Click);
+            // 
+            // btn_menuEstadisticas
+            // 
+            this.btn_menuEstadisticas.FlatAppearance.BorderSize = 0;
+            this.btn_menuEstadisticas.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_menuEstadisticas.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_menuEstadisticas.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn_menuEstadisticas.Location = new System.Drawing.Point(3, 284);
+            this.btn_menuEstadisticas.Name = "btn_menuEstadisticas";
+            this.btn_menuEstadisticas.Size = new System.Drawing.Size(184, 54);
+            this.btn_menuEstadisticas.TabIndex = 7;
+            this.btn_menuEstadisticas.Text = "Estadisticas";
+            this.btn_menuEstadisticas.UseVisualStyleBackColor = true;
+            this.btn_menuEstadisticas.Click += new System.EventHandler(this.btn_menuEstadisticas_Click);
+            // 
+            // btn_menuFranquicias
+            // 
+            this.btn_menuFranquicias.FlatAppearance.BorderSize = 0;
+            this.btn_menuFranquicias.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_menuFranquicias.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_menuFranquicias.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn_menuFranquicias.Location = new System.Drawing.Point(3, 227);
+            this.btn_menuFranquicias.Name = "btn_menuFranquicias";
+            this.btn_menuFranquicias.Size = new System.Drawing.Size(184, 54);
+            this.btn_menuFranquicias.TabIndex = 6;
+            this.btn_menuFranquicias.Text = "Franquicias";
+            this.btn_menuFranquicias.UseVisualStyleBackColor = true;
+            this.btn_menuFranquicias.Click += new System.EventHandler(this.btn_menuFranquicias_Click);
+            // 
+            // btn_menuProductos
+            // 
+            this.btn_menuProductos.FlatAppearance.BorderSize = 0;
+            this.btn_menuProductos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_menuProductos.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_menuProductos.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn_menuProductos.Location = new System.Drawing.Point(3, 113);
+            this.btn_menuProductos.Name = "btn_menuProductos";
+            this.btn_menuProductos.Size = new System.Drawing.Size(184, 54);
+            this.btn_menuProductos.TabIndex = 5;
+            this.btn_menuProductos.Text = "Productos";
+            this.btn_menuProductos.UseVisualStyleBackColor = true;
+            this.btn_menuProductos.Click += new System.EventHandler(this.btn_menuProductos_Click);
+            // 
+            // btn_menuVendedores
+            // 
+            this.btn_menuVendedores.FlatAppearance.BorderSize = 0;
+            this.btn_menuVendedores.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_menuVendedores.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_menuVendedores.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn_menuVendedores.Location = new System.Drawing.Point(3, 170);
+            this.btn_menuVendedores.Name = "btn_menuVendedores";
+            this.btn_menuVendedores.Size = new System.Drawing.Size(184, 54);
+            this.btn_menuVendedores.TabIndex = 4;
+            this.btn_menuVendedores.Text = "Vendedores";
+            this.btn_menuVendedores.UseVisualStyleBackColor = true;
+            this.btn_menuVendedores.Click += new System.EventHandler(this.btn_menuVendedores_Click);
+            // 
+            // btn_menuPedidos
+            // 
+            this.btn_menuPedidos.FlatAppearance.BorderSize = 0;
+            this.btn_menuPedidos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_menuPedidos.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_menuPedidos.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn_menuPedidos.Location = new System.Drawing.Point(3, 56);
+            this.btn_menuPedidos.Name = "btn_menuPedidos";
+            this.btn_menuPedidos.Size = new System.Drawing.Size(184, 54);
+            this.btn_menuPedidos.TabIndex = 3;
+            this.btn_menuPedidos.Text = "Pedidos";
+            this.btn_menuPedidos.UseVisualStyleBackColor = true;
+            this.btn_menuPedidos.Click += new System.EventHandler(this.btn_menuPedidos_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.YellowGreen;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(190, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(778, 24);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.YellowGreen;
+            this.panel3.Controls.Add(this.pictureBox1);
+            this.panel3.Location = new System.Drawing.Point(849, 7);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(104, 106);
+            this.panel3.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(3, 10);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(98, 93);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(254, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(553, 48);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Bienvenidos al Sistema de Administracion de pedidos,\r\n clientes y vendedores de D" +
+    "on Cayetano Pastas.\r\n";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Escritorio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(968, 499);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Escritorio";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Sistema de Administracion de pedidos, clientes y vendedores - Don Cayetano Pastas" +
+    "";
+            this.panel1.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btn_menuReportes;
+        private System.Windows.Forms.Button btn_menuEstadisticas;
+        private System.Windows.Forms.Button btn_menuFranquicias;
+        private System.Windows.Forms.Button btn_menuProductos;
+        private System.Windows.Forms.Button btn_menuVendedores;
+        private System.Windows.Forms.Button btn_menuPedidos;
+        private System.Windows.Forms.Panel SidePanel;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- TP-PAV/formularios/escritorio.cs
+++ TP-PAV/formularios/escritorio.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TP_PAV.formularios;
 
 namespace TP_PAV
 {
@@ -15,6 +16,58 @@ namespace TP_PAV
         public Escritorio()
         {
             InitializeComponent();
+            SidePanel.Hide();
+           
         }
+
+        private void btn_menuPedidos_Click(object sender, EventArgs e)
+        {
+            SidePanel.Show();
+            SidePanel.Height = btn_menuPedidos.Height;
+            SidePanel.Top = btn_menuPedidos.Top;
+
+        }
+
+      
+
+        private void btn_menuProductos_Click(object sender, EventArgs e)
+        {
+            SidePanel.Show();
+            SidePanel.Height = btn_menuProductos.Height;
+            SidePanel.Top = btn_menuProductos.Top;
+
+           frm_ABM_Producto frm_producto = new frm_ABM_Producto();
+           frm_producto.ShowDialog();
+            
+        }
+
+        private void btn_menuVendedores_Click(object sender, EventArgs e)
+        {
+            SidePanel.Show();
+            SidePanel.Height = btn_menuVendedores.Height;
+            SidePanel.Top = btn_menuVendedores.Top;
+        }
+
+        private void btn_menuFranquicias_Click(object sender, EventArgs e)
+        {
+            SidePanel.Show();
+            SidePanel.Height = btn_menuFranquicias.Height;
+            SidePanel.Top = btn_menuFranquicias.Top;
+        }
+
+        private void btn_menuEstadisticas_Click(object sender, EventArgs e)
+        {
+            SidePanel.Show();
+            SidePanel.Height = btn_menuEstadisticas.Height;
+            SidePanel.Top = btn_menuEstadisticas.Top;
+        }
+
+        private void btn_menuReportes_Click(object sender, EventArgs e)
+        {
+            SidePanel.Show();
+            SidePanel.Height = btn_menuReportes.Height;
+            SidePanel.Top = btn_menuReportes.Top;
+        }
+
     }
 }
