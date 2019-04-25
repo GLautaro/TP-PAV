fdf2fdd72cc4bd23add586dec81e9630d58a30dc
vivascastillomatias <mativivas77>
2019-04-25 19:43:10 -0300

    Modificacion de texto de bienvenida

--- TP-PAV/formularios/escritorio.Designer.cs
+++ TP-PAV/formularios/escritorio.Designer.cs
@@ -190,10 +190,10 @@
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(207, 45);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(998, 25);
+            this.label1.Size = new System.Drawing.Size(851, 25);
             this.label1.TabIndex = 3;
-            this.label1.Text = "Bienvenidos al Sistema de Administracion de pedidos, clientes y vendedores de Don" +
-    " Cayetano Pastas.\r\n";
+            this.label1.Text = "Sistema de Administracion de pedidos, clientes y vendedores de Don Cayetano Pasta" +
+    "s.\r\n";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // main_panel
