cc4a0e089155ebf1c83e330f9db630c705c08224
GLautaro <glautaroivan@gmail.com>
2019-04-28 20:29:26 -0300

    (PARCIAL) ABM Producto
    Agregadas funcionalidades de los botones y comportamientos.
    Falta el Alta y Modificacion de productos

--- TP-PAV/clases/Producto.cs
+++ TP-PAV/clases/Producto.cs
@@ -3,10 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
 
 namespace TP_PAV.clases
 {
     class Producto
     {
+        AccesoBD db = new AccesoBD();
+
+        public DataTable traerProductos()
+        {
+            string consulta = @"SELECT * FROM producto";
+
+            return db.ejecutarConsulta(consulta);
+        }
+
+        public DataTable buscarProductos(string texto)
+        {
+            string consulta = @"SELECT * FROM producto WHERE nombre_producto LIKE '%" + texto + "%' OR id_producto LIKE '%" + texto + "%';";
+
+            return db.ejecutarConsulta(consulta);
+        }
+
+/*        public DataTable insertarproducto(string nombre, string cantidad, string id_tipo_producto)
+        {
+            string consulta = @"insert into producto (nombre_producto, cantidad_u_medida, 
+                                id_u_medida, descripcion, id_tipo_producto, precio_unitario, estado_producto) 
+                                values ('" + nombre + "','" + cantidad + "','" + id_tipo_producto + "')";
+            return db.ejecutarConsulta(consulta);
+        }
+*/       
+        
     }
 }
--- TP-PAV/formularios/uc_ABM_Producto.cs
+++ TP-PAV/formularios/uc_ABM_Producto.cs
@@ -7,12 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TP_PAV.clases;
 
 namespace TP_PAV.formularios
 {
     public partial class uc_ABM_Producto : UserControl
     {
-
+        Producto producto = new Producto();
         private static uc_ABM_Producto priv_instance;
         public static uc_ABM_Producto pub_instance
         {
@@ -31,6 +32,11 @@ namespace TP_PAV.formularios
             InitializeComponent();
         }
 
+        private void uc_ABM_Producto_Load(object sender, EventArgs e)
+        {
+            this.dgv_productos.DataSource = producto.traerProductos();
+        }
+
         private void btn_agregarTipo_Click(object sender, EventArgs e)
         {
             frm_ABM_TipoProducto frm_ABM_TipoProducto = new frm_ABM_TipoProducto();
@@ -42,10 +48,6 @@ namespace TP_PAV.formularios
 
         }
 
-        private void btn_guardarModProducto_Click(object sender, EventArgs e)
-        {
-           
-        }
 
         private void btn_agregarUdeMedida_Click(object sender, EventArgs e)
         {
@@ -53,9 +55,68 @@ namespace TP_PAV.formularios
             frm_ABM_UnidadMedida.ShowDialog();
         }
 
-        private void uc_ABM_Producto_Load(object sender, EventArgs e)
-        {
 
+        private void btn_registrarProducto_Click(object sender, EventArgs e)
+        {
+            this.btn_modificarProducto.Visible = false;
+            this.btn_guardarModProducto.Visible = true;
+            this.btn_cancelarModProducto.Visible = true;
+            cleanTextBox();
+            modificarCajasDeTxt(true);
         }
+
+        private void btn_modificarProducto_Click(object sender, EventArgs e)
+        {
+            this.btn_modificarProducto.Visible = false;
+            this.btn_guardarModProducto.Visible = true;
+            this.btn_cancelarModProducto.Visible = true;
+            cleanTextBox();
+            modificarCajasDeTxt(true);
+        }
+
+        private void btn_guardarModProducto_Click(object sender, EventArgs e)
+        {
+            cleanTextBox();
+            modificarCajasDeTxt(false);
+            this.btn_guardarModProducto.Visible = false;
+            this.btn_cancelarModProducto.Visible = false;
+            this.btn_modificarProducto.Visible = true;
+            this.btn_registrarProducto.Visible = true;
+        }
+
+        private void btn_cancelarModProducto_Click(object sender, EventArgs e)
+        {
+            cleanTextBox();
+            modificarCajasDeTxt(false);
+            this.btn_guardarModProducto.Visible = false;
+            this.btn_cancelarModProducto.Visible = false;
+            this.btn_modificarProducto.Visible = true;
+            this.btn_registrarProducto.Visible = true;
+            
+        }
+
+
+        private void modificarCajasDeTxt(bool estado)
+        {
+            this.txt_NombreProducto.Enabled = estado;
+            this.txt_cantidadProducto.Enabled = estado;
+            this.cmb_unidadMedida.Enabled = estado;
+            this.txt_descripcionProducto.Enabled = estado;
+            this.cmb_tipoProducto.Enabled = estado;
+            this.txt_precioUnitario.Enabled = estado;
+            this.cmb_estadoProducto.Enabled = estado;
+            this.btn_agregarTipo.Enabled = estado;
+            this.btn_agregarUdeMedida.Enabled = estado;
+        }
+
+        private void cleanTextBox() 
+        {
+            this.txt_NombreProducto.Clear();
+            this.txt_cantidadProducto.Clear();
+            this.txt_descripcionProducto.Clear();
+            this.txt_precioUnitario.Clear();
+        }
+
+        
     }
 }
